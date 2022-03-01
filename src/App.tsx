0591942215b2c2e06d6db005fd2a0f3f3f8c3611
Kapildev Neupane <kapildevneupane55@gmail.com>
2022-03-01 16:28:35 +0545

    Add search input

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,36 @@ function App(): React.ReactElement {
   return (
     <div style={{ backgroundColor: "#F5F6F7" }} className="h-screen font-sans">
       <div className="h-10" />
-      <div className="w-4/6 mx-auto">
-        <div className="rounded-3xl border-2 border-gray-200 h-20 bg-white flex flex-row items-center justify-between p-5">
-          <p className="text-gray-400">Comment or type '/' for comments</p>
-          <div className="text-gray-400 space-x-2 flex flex-row items-center content-center">
-            <span>@</span>
-            <span className="font-bold">B</span>
-            <span className="italic font-serif">I</span>
-            <span className="underline">U</span>
-            <DotsHorizontalIcon className="h-6 w-6" />
-          </div>
-        </div>
-      </div>
+      {SeachInput()}
     </div>
   );
 }
 
 export default App;
+
+function SeachInput() {
+  return (
+    <div className="w-4/6 mx-auto">
+      <div className="flex flex-row items-center justify-between relative">
+        <input
+          type="text"
+          className="placeholder:text-gray-400 focus:outline-none w-full h-20 bg-white rounded-3xl p-5 border-2 border-gray-200  focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+          placeholder="Comment or type '/' for comments"
+        />
+        <Actions />
+      </div>
+    </div>
+  );
+}
+
+function Actions() {
+  return (
+    <div className="text-gray-400 space-x-2 flex flex-row items-center content-center absolute right-5">
+      <span>@</span>
+      <span className="font-bold">B</span>
+      <span className="italic font-serif">I</span>
+      <span className="underline">U</span>
+      <DotsHorizontalIcon className="h-6 w-6" />
+    </div>
+  );
+}
