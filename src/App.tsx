500346fbd7c7beeb494a3e9ab3850faee76f357f
Kapildev Neupane <kapildevneupane55@gmail.com>
2022-03-01 16:09:55 +0545

    Add input for adding comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { DotsHorizontalIcon } from "@heroicons/react/solid";
 
-function App() {
+function App(): React.ReactElement {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div style={{ backgroundColor: "#F5F6F7" }} className="h-screen font-sans">
+      <div className="h-10" />
+      <div className="w-4/6 mx-auto">
+        <div className="rounded-3xl border-2 border-gray-200 h-20 bg-white flex flex-row items-center justify-between p-5">
+          <p className="text-gray-400">Comment or type '/' for comments</p>
+          <div className="text-gray-400 space-x-2 flex flex-row items-center content-center">
+            <span>@</span>
+            <span className="font-bold">B</span>
+            <span className="italic font-serif">I</span>
+            <span className="underline">U</span>
+            <DotsHorizontalIcon className="h-6 w-6" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
