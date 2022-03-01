54c9ff3bce44c66563bac4a75e46e9d125e4901b
Kapildev Neupane <kapildevneupane55@gmail.com>
2022-03-01 16:59:08 +0545

    Add Activity Log Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,81 @@
 import React from "react";
-import { DotsHorizontalIcon } from "@heroicons/react/solid";
+import { DotsHorizontalIcon, AdjustmentsIcon } from "@heroicons/react/solid";
 
 function App(): React.ReactElement {
   return (
     <div style={{ backgroundColor: "#F5F6F7" }} className="h-screen font-sans">
-      <div className="h-10" />
-      <SearchInput />
+      <div className="w-4/6 mx-auto">
+        <Spacer />
+        <SearchInput />
+        <Spacer />
+        <ActivityLogHeader />
+      </div>
     </div>
   );
 }
 
 export default App;
 
+function ActivityLogHeader() {
+  return (
+    <div className="flex flex-row justify-between items-center">
+      <ActivityLogTitle />
+      <ActivityLogActions />
+    </div>
+  );
+}
+
+function ActivityLogActions() {
+  return (
+    <div className="text-gray-400 flex flex-row space-x-4">
+      <ShowAllActivityToggle />
+      <AdjustmentIconButton />
+    </div>
+  );
+}
+
+function AdjustmentIconButton() {
+  return (
+    <div className="w-10 h-10 rounded-full border-2 border-gray-300 p-1 bg-white">
+      <AdjustmentsIcon />
+    </div>
+  );
+}
+
+function ShowAllActivityToggle() {
+  return (
+    <div className="flex flex-row space-x-3 items-center">
+      <span className="text-gray-600">Show all activity</span>
+      <Switch />
+    </div>
+  );
+}
+
+function Switch() {
+  return (
+    <div className="w-16 h-8 bg-gray-300 rounded-full flex">
+      <div className="w-6 h-6 bg-white rounded-full self-center ml-1" />
+    </div>
+  );
+}
+
+function ActivityLogTitle() {
+  return <h2 className="text-3xl font-bold">Activity logs</h2>;
+}
+
+function Spacer() {
+  return <div className="h-10" />;
+}
+
 function SearchInput() {
   return (
-    <div className="w-4/6 mx-auto">
-      <div className="flex flex-row items-center justify-between relative text-lg">
-        <input
-          type="text"
-          className="placeholder:text-gray-400 focus:outline-none w-full h-20 bg-white rounded-3xl p-5 border-2 border-gray-200  focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
-          placeholder="Comment or type '/' for comments"
-        />
-        <Actions />
-      </div>
+    <div className="flex flex-row items-center justify-between relative text-lg">
+      <input
+        type="text"
+        className="placeholder:text-gray-400 focus:outline-none w-full h-20 bg-white rounded-3xl p-5 border-2 border-gray-200  focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+        placeholder="Comment or type '/' for comments"
+      />
+      <Actions />
     </div>
   );
 }
