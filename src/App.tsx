e5b1bf1942ad840898a39bc29459a7897f20d3d7
Kapildev Neupane <kapildevneupane55@gmail.com>
2022-03-01 16:31:34 +0545

    Change Search input's font size

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ function App(): React.ReactElement {
   return (
     <div style={{ backgroundColor: "#F5F6F7" }} className="h-screen font-sans">
       <div className="h-10" />
-      {SeachInput()}
+      <SearchInput />
     </div>
   );
 }
 
 export default App;
 
-function SeachInput() {
+function SearchInput() {
   return (
     <div className="w-4/6 mx-auto">
-      <div className="flex flex-row items-center justify-between relative">
+      <div className="flex flex-row items-center justify-between relative text-lg">
         <input
           type="text"
           className="placeholder:text-gray-400 focus:outline-none w-full h-20 bg-white rounded-3xl p-5 border-2 border-gray-200  focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
